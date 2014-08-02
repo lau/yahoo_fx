f653fb83ff9425fd6f64215a2c74c0857944e1ad
Lau Taarnskov <lau@taarnskov.com>
2014-08-02 13:35:17 +0200

    add package information to mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule YahooFx.Mixfile do
     [app: :yahoo_fx,
      version: "0.1.0",
      elixir: "~> 0.14.3",
+     package: package,
      deps: deps]
   end
 
@@ -14,8 +15,14 @@ defmodule YahooFx.Mixfile do
 
   defp deps do
     [
-      { :httpotion, "~> 0.2.4" },
-      { :ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.0" },
+      {:httpotion, "~> 0.2.4" },
+      {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.0"},
     ]
+  end
+
+  defp package do
+    %{ licenses: ["MIT"],
+       contributors: ["Lau Taarnskov"],
+       links: %{ "GitHub" => "https://github.com/lau/yahoo_fx" } }
   end
 end
